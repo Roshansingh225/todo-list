5294b52f94ce5faf774f2bffac79416aa4135b68
Roshansingh225 <129258347+Roshansingh225@users.noreply.github.com>
2025-09-03 19:54:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const bodyParser = require("body-parser");
+const express=require("express");
+
+var app= express();
+
+app.set("view engine","ejs");
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+
+var items=[];
+var emp="working";
+
+app.get("/",function(req,res){
+    res.render("list",{ejes:items})
+});
+
+app.post("/",function(req,res){
+    var item =req.body.ele1;
+    items.push(item);
+    res.redirect("/");
+});
+
+
+
+app.listen(3000,function(){console.log("server started")})
